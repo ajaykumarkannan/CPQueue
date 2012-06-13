962144cc6679689f9b2581da3cd130e002176b70
unknown <ajaykuma@c-fsoban.ap.qualcomm.com>
2012-06-13 10:35:59 +0530

    Initial git commit

---
+++ CPQueue/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+using System.Runtime.InteropServices;
+
+namespace CPQueue
+{
+    partial class Form1
+    {
+        [DllImport("User32.dll")]
+        protected static extern int SetClipboardViewer(int hWndNewViewer);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd,
+          int id, int fsModifiers, int vlc);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            ChangeClipboardChain(this.Handle, nextClipboardViewer);
+
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        /// 
+
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewHelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.AllowColumnReorder = true;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.LabelEdit = true;
+            this.listView1.Location = new System.Drawing.Point(12, 61);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(531, 170);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.myListview_MouseDown);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 239);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(236, 19);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Copy clicked item to buffer and remove";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(456, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 27);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(525, 15);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "This application will copy text automatically. To load one of the items into the " +
+    "buffer, click it once.";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(560, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewHelpToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // viewHelpToolStripMenuItem
+            // 
+            this.viewHelpToolStripMenuItem.Name = "viewHelpToolStripMenuItem";
+            this.viewHelpToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.viewHelpToolStripMenuItem.Text = "View Help";
+            this.viewHelpToolStripMenuItem.Click += new System.EventHandler(this.viewHelpToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(333, 234);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 27);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Remove Selected";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 300);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 15);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "label2";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 265);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(215, 19);
+            this.checkBox2.TabIndex = 16;
+            this.checkBox2.Text = "Tick to Use Stack instead of Queue";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 324);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Opacity = 0.85D;
+            this.Text = "CPQueue";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private Label label1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem optionsToolStripMenuItem;
+        private ToolStripMenuItem settingsToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem viewHelpToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private Button button3;
+        private Label label2;
+        private CheckBox checkBox2;
+    }
+}
+
---
+++ CPQueue/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+using System.Windows.Input;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+
+// Clipboard Monitor Code adapted from: http://www.codeguru.com/columns/dotnettips/article.php/c7315/Monitoring-Clipboard-Activity-in-C.htm#overview
+// Hotkey from http://www.codeproject.com/Articles/5914/Simple-steps-to-enable-Hotkey-and-ShortcutInput-us
+
+namespace CPQueue
+{
+    public partial class Form1 : Form
+    {
+
+        string mstring;
+
+        IntPtr nextClipboardViewer;
+        int selectedItem = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            nextClipboardViewer = (IntPtr)SetClipboardViewer((int)this.Handle);
+            this.listView1.Clear();
+            this.listView1.Columns.Add("Text", 450);
+            this.listView1.Columns.Add("Added", 80);
+            this.listView1.HideSelection = false;
+            
+
+            // Alt = 1, Ctrl = 2, Shift = 4, Win = 8
+            RegisterHotKey(this.Handle, this.GetType().GetHashCode(), 2, (int)'B');
+            RegisterHotKey(this.Handle, this.GetType().GetHashCode(), 2, (int)'E');
+            RegisterHotKey(this.Handle, this.GetType().GetHashCode(), 2, (int)'D');
+            // UnregisterHotKey(this.Handle, this.GetType().GetHashCode());
+            label2.Text = "Shortcuts: Ctrl-B: Delete selected, Ctrl-E: Move up, Ctrl-D: Move Down";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        /*
+        // Add button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mstring = Clipboard.GetText();
+
+            // Insert ListView Code here
+            ListViewItem lvi = new ListViewItem(mstring);
+            lvi.SubItems.Add(DateTime.Now.ToString("HH:mm:ss tt"));
+            listView1.Items.Add(lvi);
+
+        }
+        */
+
+        void myListview_MouseDown(Object sender, MouseEventArgs e)
+        {
+            // Make sure it was a single left click, like the normal Click event
+            if ((e.Button == MouseButtons.Left) && (e.Clicks == 1))
+            {
+                try
+                {
+                    ListViewHitTestInfo htInfo = listView1.HitTest(e.X, e.Y);
+                    mstring = htInfo.Item.Text;
+                    Clipboard.SetData(DataFormats.Text, (Object)mstring);
+                    if (checkBox1.Checked && listView1.Items.Count > 0)
+                    {
+                        listView1.Items.RemoveAt(htInfo.Item.Index);
+                    }
+                    else
+                    {
+                        selectedItem = htInfo.Item.Index;
+                    }
+                }
+                catch (NullReferenceException ex)
+                {
+                    ; // Do nothing
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Clear Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Clear();
+            this.listView1.Columns.Add("Text", 450);
+            this.listView1.Columns.Add("Added", 80);
+            selectedItem = -1;
+
+        }
+
+        protected override void WndProc(ref System.Windows.Forms.Message m)
+        {
+            // defined in winuser.h
+            const int WM_DRAWCLIPBOARD = 0x308;
+            const int WM_CHANGECBCHAIN = 0x030D;
+            const int WM_HOTKEY = 0x0312; 
+
+            switch (m.Msg)
+            {
+                case WM_HOTKEY:
+                    Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);
+                    if (key == Keys.B)
+                    {
+                        if (listView1.Items.Count > 0)
+                        {
+                            listView1.Items.RemoveAt(selectedItem);
+                            if (listView1.Items.Count > 0)
+                            {
+                                if (!checkBox2.Checked)
+                                {
+                                    if (selectedItem < listView1.Items.Count - 1) ; // Stays the same
+                                    else selectedItem--;
+                                }
+                                else
+                                {
+                                    if (selectedItem > 0) selectedItem--;
+                                    // else stays the same
+                                }
+
+                                loadItem();
+                            }
+                        }
+                    }
+                    else if (key == Keys.E)
+                    {
+                        if (selectedItem > 0)
+                        {
+                            listView1.Items[selectedItem].Selected = false;
+                            selectedItem--;
+                            loadItem();
+                        }
+                    }
+                    else if (key == Keys.D)
+                    {
+                        if (selectedItem < listView1.Items.Count - 1)
+                        {
+                            listView1.Items[selectedItem].Selected = false;
+                            selectedItem++;
+
+                            loadItem();
+                        }
+                    }
+                    base.WndProc(ref m);
+                    break;
+                case WM_DRAWCLIPBOARD:
+                    DisplayClipboardData();
+                    SendMessage(nextClipboardViewer, m.Msg, m.WParam, m.LParam);
+                    break;
+                case WM_CHANGECBCHAIN:
+                    if (m.WParam == nextClipboardViewer)
+                        nextClipboardViewer = m.LParam;
+                    else
+                        SendMessage(nextClipboardViewer, m.Msg, m.WParam, m.LParam);
+                    break;
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+
+        void DisplayClipboardData()
+        {
+            try
+            {
+                IDataObject iData = new DataObject();
+                iData = Clipboard.GetDataObject();
+
+                if (iData.GetDataPresent(DataFormats.Text))
+                {
+                    mstring = (string)iData.GetData(DataFormats.Text);
+                    ListViewItem tLVI = listView1.FindItemWithText(mstring);
+                    if (tLVI == null || tLVI.Text != mstring)
+                    {
+                        ListViewItem lvi = new ListViewItem(mstring);
+                        lvi.SubItems.Add(DateTime.Now.ToString("HH:mm:ss tt"));
+                        listView1.Items.Add(lvi);
+                        listView1.Items[selectedItem].Selected = false; 
+                        if (!checkBox2.Checked)
+                        {
+                            selectedItem = 0;
+                            loadItem();
+                        }
+                        else
+                        {
+                            selectedItem = listView1.Items.Count - 1;
+                            loadItem();
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Remove selected button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count > 0)
+            {
+                listView1.Items.RemoveAt(selectedItem);
+                if (listView1.Items.Count > 0)
+                {
+                    if (!checkBox2.Checked)
+                    {
+                        if (selectedItem < listView1.Items.Count - 1) ; // Stays the same
+                        else selectedItem--;
+                    }
+                    else
+                    {
+                        if (selectedItem > 0) selectedItem--;
+                        // else stays the same
+                    }
+
+                    loadItem();
+                    
+                }
+            }
+        }
+
+        private void viewHelpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            listView1.Items[selectedItem].Selected = false; 
+            if (checkBox2.Checked) selectedItem = listView1.Items.Count - 1;
+            else selectedItem = 0;
+            loadItem();
+
+        }
+
+        private void loadItem()
+        {
+            mstring = listView1.Items[selectedItem].Text;
+            Clipboard.SetData(DataFormats.Text, (Object)mstring);
+            listView1.Items[selectedItem].Selected = true;
+            listView1.EnsureVisible(selectedItem);
+        }
+    }
+}
